f7e48a108f80f6c10c19ab37d92519e03d1a506f
Yousef Mahmoud <usef.mahmoud@outlook.com>
2023-04-17 14:38:01 +0200

    remove some fonts and heroku config

--- funcs.py
+++ funcs.py
@@ -1,12 +1,17 @@
-import requests, os, textwrap
+import requests
+import os
+import textwrap
 from PIL import Image, ImageDraw, ImageFont
 import ffmpeg
 
-#muhammadjibreel
-#shaatree
+# muhammadjibreel
+# shaatree
+# alafasy
+
 
 def get_data(surah, ayah):
-    res = requests.get(f'http://api.alquran.cloud/v1/ayah/{surah}:{ayah}/ar.shaatree').json()
+    res = requests.get(
+        f'http://api.alquran.cloud/v1/ayah/{surah}:{ayah}/ar.alafasy').json()
 
     return {
         'audio': res['data']['audio'],
@@ -14,9 +19,10 @@ def get_data(surah, ayah):
         'surah': res['data']['surah']['name']
     }
 
+
 def craete_image(text, ayah_id):
     font = ImageFont.truetype('data/fonts/UthmanicHafs.ttf', 70)
-    
+
     lines = textwrap.wrap(text, width=115)
     lines_h = 0
     for line in lines:
@@ -25,18 +31,21 @@ def craete_image(text, ayah_id):
 
     img = Image.new('RGB', (1920, lines_h+300+((len(lines))*20)))
     draw = ImageDraw.Draw(img)
-    
+
     font_top = 170
     for line in lines:
         w, h = font.getsize(line)
+
         draw.text(((img.width-w)/2, font_top), line, font=font, align='center')
         font_top += (h+20)
 
     img.save(f'data/images/ayah_{ayah_id}.png')
 
+
 def download_audio(url, ayah_id):
     res = requests.get(url)
     open(f'data/audio/ayah_{ayah_id}.mp3', 'wb').write(res.content)
+
 
 def combine_audio_image(audio_path, image_path, output_file):
 
